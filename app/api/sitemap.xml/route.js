eda786b6ac2ee85fc1d170bc4003a4ff29101dd1
Eisha Anwar <eishaanwar51@gmail.com>
2025-07-20 21:40:05 +0500

    sitemap.xml

---
+++ app/api/sitemap.xml/route.js
@@ -0,0 +1,33 @@
+export async function GET() {
+  const baseUrl = "https://eishaanwar.me"; // Replace with your live domain
+
+  const pages = [
+    "",
+    "/about",
+    "/projects",
+    "/skills",
+    "/certification",
+    "/awards",
+    "/contact",
+  ];
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${pages
+      .map(
+        (page) => `
+      <url>
+        <loc>${baseUrl}${page}</loc>
+        <changefreq>weekly</changefreq>
+        <priority>0.8</priority>
+      </url>`
+      )
+      .join("")}
+  </urlset>`;
+
+  return new Response(sitemap, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
+}
